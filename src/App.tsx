14e04a227bee7866f68e3adc6896e749a44a7beb
Alex <72755893+mikromajor@users.noreply.github.com>
2022-12-18 22:02:47 +0100

    took out the AlcoCalendar from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,6 @@
-import {
-  useAppSelector,
-  useAppDispatch,
-} from "./store/hooks/redux";
-import { alcoActions } from "./store/reducer/alcoReducer";
-import {
-  ControlPanel,
-  MainPanel,
-  Cleaner,
-  Multiplier,
-} from "./components";
+import { AlcoCalendar } from "./components";
 
-function App() {
-  const { alcoReducer } = useAppSelector((state) => state);
-  const {
-    additionPercent,
-    additionVolume,
-    subtractionPercent,
-    subtractionVolume,
-  } = alcoActions;
-
-  return (
-    <div className='calendar'>
-      <Multiplier />
-      <ControlPanel
-        indicatorName={"liters"}
-        indicatorValue={alcoReducer.liters}
-        subtractCallback={subtractionVolume}
-        addCallback={additionVolume}
-      />
-      <ControlPanel
-        indicatorName={"percent"}
-        indicatorValue={alcoReducer.percent}
-        subtractCallback={subtractionPercent}
-        addCallback={additionPercent}
-      />
-      <MainPanel />
-      <Cleaner />
-    </div>
-  );
-}
+const App = () => {
+  return <AlcoCalendar />;
+};
 export default App;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { MainPanel } from "./MainPanel/MainPanel";
 export { Cleaner } from "./Cleaner/Cleaner";
 export { ChosenMonthInfo } from "./ChosenMonthInfo/ChosenMonthInfo";
 export { Multiplier } from "./Multiplier/Multiplier";
+export { AlcoCalendar } from "./AlcoCalendar/AlcoCalendar";
