d3b0652c2d20070b6541cb675075fc59d50588d2
Alex <72755893+mikromajor@users.noreply.github.com>
2023-04-24 20:39:54 +0200

    styled

--- src/components/Salary/Salary.tsx
+++ src/components/Salary/Salary.tsx
@@ -8,76 +8,71 @@ export const Salary = () => {
   );
 
   return (
-    <>
-      <h1>Salary</h1>
-
+    <div className='salary'>
       <table>
+        <caption>Salary</caption>
         <TableHeadings />
-        <tbody>
-          <tr>
-            <td>
-              <Input payloadsKey={PayloadsKeys.year} />
-            </td>
-            <td>
-              <Input payloadsKey={PayloadsKeys.month} />
-            </td>
-            <td>
-              <Input
-                payloadsKey={PayloadsKeys.salaryRate}
-              />
-            </td>
-            <td>
-              <Input payloadsKey={PayloadsKeys.taxRate} />
-            </td>
-            <td>
-              <Input
-                payloadsKey={PayloadsKeys.premiumRate}
-              />
-            </td>
-            <td> {salaryReducer.weekDays} </td>
-            <td>{salaryReducer.weekendDays}</td>
-            <td>
-              <Input payloadsKey={PayloadsKeys.holidays} />
-            </td>
-            <td> {salaryReducer.standardWorkHours}</td>
-            <td>
-              <Input
-                payloadsKey={PayloadsKeys.extraHours_50}
-              />
-            </td>
-            <td>
-              <Input
-                payloadsKey={PayloadsKeys.extraHours_100}
-              />
-            </td>
-            <td>
-              <Input
-                payloadsKey={PayloadsKeys.sickLeaveWeekDays}
-              />
-            </td>
-            <td>
-              <Input
-                payloadsKey={
-                  PayloadsKeys.sickLeaveWeekendDays
-                }
-              />
-            </td>
-            <td>
-              <Input
-                payloadsKey={PayloadsKeys.usedVacation}
-              />
-            </td>
-            <td>
-              <Input
-                payloadsKey={PayloadsKeys.bloodDonation}
-              />
-            </td>
-            <td>{salaryReducer.standardSalary}</td>
-            <td>{salaryReducer.extraSalary}</td>
-            <td>{salaryReducer.totalSalary}</td>
-          </tr>
-        </tbody>
+        <tr>
+          <td>
+            <Input payloadsKey={PayloadsKeys.year} />
+          </td>
+          <td>
+            <Input payloadsKey={PayloadsKeys.month} />
+          </td>
+          <td>
+            <Input payloadsKey={PayloadsKeys.salaryRate} />
+          </td>
+          <td>
+            <Input payloadsKey={PayloadsKeys.taxRate} />
+          </td>
+          <td>
+            <Input payloadsKey={PayloadsKeys.premiumRate} />
+          </td>
+          <td> {salaryReducer.weekDays} </td>
+          <td>{salaryReducer.weekendDays}</td>
+          <td>
+            <Input payloadsKey={PayloadsKeys.holidays} />
+          </td>
+          <td> {salaryReducer.standardWorkHours}</td>
+          <td>
+            <Input
+              payloadsKey={PayloadsKeys.extraHours_50}
+            />
+          </td>
+          <td>
+            <Input
+              payloadsKey={PayloadsKeys.extraHours_100}
+            />
+          </td>
+          <td>
+            <Input
+              payloadsKey={PayloadsKeys.sickLeaveWeekDays}
+            />
+          </td>
+          <td>
+            <Input
+              payloadsKey={
+                PayloadsKeys.sickLeaveWeekendDays
+              }
+            />
+          </td>
+          <td>
+            <Input
+              payloadsKey={PayloadsKeys.usedVacation}
+            />
+          </td>
+          <td>
+            <Input
+              payloadsKey={PayloadsKeys.bloodDonation}
+            />
+          </td>
+          <td>{salaryReducer.standardSalary}</td>
+          <td>{salaryReducer.extraSalary}</td>
+          <td className='salary-all'>
+            {salaryReducer.totalSalary}
+          </td>
+        </tr>
       </table>
-    </>
+    </div>
   );
 };
--- src/ui/TableHeadings/TableHeadings.tsx
+++ src/ui/TableHeadings/TableHeadings.tsx
@@ -2,12 +2,10 @@ import { TABLE_HEADINGS } from "../../store/reducer/constants/salaryConstants";
 
 export const TableHeadings = () => {
   return (
-    <thead>
-      <tr>
-        {TABLE_HEADINGS.map((heading, i) => (
-          <th key={heading + String(i)}>{heading}</th>
-        ))}
-      </tr>
-    </thead>
+    <tr>
+      {TABLE_HEADINGS.map((heading, i) => (
+        <th key={heading + String(i)}>{heading}</th>
+      ))}
+    </tr>
   );
 };
