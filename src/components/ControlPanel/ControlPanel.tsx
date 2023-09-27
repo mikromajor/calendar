67f37622b741d4550c365b94f645543411ffb930
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2023-09-27 22:51:14 +0200

    TODO fix ControlPanel

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,27 +1,48 @@
-import { ActionCreatorWithoutPayload } from "@reduxjs/toolkit";
+// import { ActionCreatorWithoutPayload } from "@reduxjs/toolkit";
 import {
   useAppSelector,
   useAppDispatch,
 } from "../../store/hooks/redux";
+import { alcoActions } from "../../store/reducer/alcoReducer";
+import { Payload } from "../../store/reducer/types/alcoTypes";
 
 export const ControlPanel = () => {
   const dispatch = useAppDispatch();
   const { alcoReducer } = useAppSelector((state) => state);
+  const {
+    month,
+    year,
+    percentDrunk,
+    volumeDrunks,
+    totalClearAlcoholPerMonth,
+    totalClearAlcoholPerYear,
+    totalVodkaPerMonth,
+    totalVodkaPerYear,
+  } = alcoReducer;
+
+  const {
+    changeDate,
+    changeVolumeDrunk,
+    changePercentDrunk,
+    calculating,
+  } = alcoActions;
+
   return (
     <div className='calendar-controlPanel'>
       <label>
         Wybież miesiąc:{" "}
         <input
           name='dataForMonth'
-          //  value={userMonth}
-          //  onChange={e => dispatch(changeMonth(e.target.value))}
+          value={month}
+          // TODO : changeDate принмает обьект а не переменную
+          //   onChange={e => dispatch(changeDate(e.target.value as unknown as Payload))}
         />
       </label>
       <label>
-        Dodavana ilość spożytego alkoholu `(względnie +,-)`:{" "}
+        Dodavana ilość spożytego alkoholu `(+,-)`:{" "}
         <input
           name='changeVolumeDrunk'
-          //  value={volumeAddingDrunk}
+          //  value={changeVolumeDrunk}
           //  onChange={e => dispatch(changeVolumeAddingDrunk(e.target.value))}
         />
       </label>
