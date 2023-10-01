2ea498e3b2391d64379c68b188fc64cdc478efca
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2023-10-01 23:22:37 +0200

    todo: upgrade alcoReducer

--- src/store/reducer/alcoReducer.ts
+++ src/store/reducer/alcoReducer.ts
@@ -21,6 +21,7 @@ export const alcoReducer = createSlice({
       action: PayloadAction<string>
     ) => {
       const volume = action.payload;
+
       !!volume && (state.volumeDrunks = Number(volume));
     },
     changePercentDrunk: (
@@ -32,21 +33,25 @@ export const alcoReducer = createSlice({
     },
     changeMonth: (state, action: PayloadAction<string>) => {
       const month = action.payload;
-      !!month &&
-        Number(month) > 0 &&
-        Number(month) < 13 &&
-        (state.month = month);
-
-      changeStateUsingStor(state, fillMonthDataWithZeros);
+      const index = state.months.findIndex(
+        (obj) => obj.month === month
+      );
+      if (index > -1) {
+        state.currentIndex = index;
+      } else {
+        state.currentIndex = state.months.length;
+        state.months.push({
+          month: month,
+          sumEthanolPerMonth: 0,
+        });
+      }
     },
     changeYear: (state, action: PayloadAction<string>) => {
       const year = action.payload;
-      !!year &&
-        Number(year) > 2020 &&
-        Number(year) < 2050 &&
-        (state.year = year);
 
-      changeStateUsingStor(state, fillStateWithZeros);
+      // шукаємо чи немає такого року в сторі
+      // якщо є то змінюємо карентРік
+      // якщо немає то зберігаємо в стор стейт за новий рік
     },
 
     calculating: (state) => {
@@ -54,38 +59,37 @@ export const alcoReducer = createSlice({
       let vodka = 0,
         ethanol = 0;
 
-      if (volumeDrunks && percentDrunk) {
-        ethanol = setDecimal(
-          (volumeDrunks * percentDrunk) / 100,
-          2
-        );
+      // if (volumeDrunks && percentDrunk) {
+      //   ethanol = setDecimal(
+      //     (volumeDrunks * percentDrunk) / 100,
+      //     2
+      //   );
 
-        vodka = setDecimal(ethanol * 2.4, 2);
+      //   vodka = setDecimal(ethanol * 2.4, 2);
 
-        state.totalEthanolPerMonth = setDecimal(
-          state.totalEthanolPerMonth + ethanol,
-          2
-        );
-        state.totalVodkaPerMonth = setDecimal(
-          state.totalVodkaPerMonth + vodka,
-          2
-        );
+      //   state.totalEthanolPerMonth = setDecimal(
+      //     state.totalEthanolPerMonth + ethanol,
+      //     2
+      //   );
+      //   state.totalVodkaPerMonth = setDecimal(
+      //     state.totalVodkaPerMonth + vodka,
+      //     2
+      //   );
 
-        state.totalVodkaPerYear = setDecimal(
-          vodka + state.totalVodkaPerYear,
-          2
-        );
-        state.totalEthanolPerYear = setDecimal(
-          ethanol + state.totalEthanolPerYear,
-          2
-        );
-      }
-      saveDataInStorage(state);
+      //   state.totalVodkaPerYear = setDecimal(
+      //     vodka + state.totalVodkaPerYear,
+      //     2
+      //   );
+      //   state.totalEthanolPerYear = setDecimal(
+      //     ethanol + state.totalEthanolPerYear,
+      //     2
+      //   );
+      // }
+      // saveDataInStorage(state);
     },
     clearStorageForMonth: (state) => {
-      const key = createKey(state.month, state.year);
-
-      window.localStorage.removeItem(key);
+      // const key = createKey(state.month, state.year);
+      // window.localStorage.removeItem(key);
     },
     // clearAllStor: () => {
     //   window.localStorage.clear();
