3a06c1e60fa704cc338a56d2d2889829d7eaddfc
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2023-10-01 14:56:31 +0200

    fix: add old ui

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { ControlPanel } from "./ControlPanel/ControlPanel";
 export { Display } from "./Display/Display";
 export { Cleaner } from "./Cleaner/Cleaner";
-export { ChosenMonthInfo } from "./ChosenMonthInfo/ChosenMonthInfo";
 export { AlcoCalendar } from "./AlcoCalendar/AlcoCalendar";
 export { Salary } from "./Salary/Salary";
---
+++ src/ui/Input/Input.tsx
@@ -0,0 +1,40 @@
+import {
+  useAppSelector,
+  useAppDispatch,
+} from "../../store/hooks/redux";
+
+import { salaryActions } from "../../store/reducer/salaryReducer";
+import { KeysSalaryInit } from "../../store/reducer/types/salaryTypes";
+
+type InputProps = {
+  payloadsKey: KeysSalaryInit;
+};
+
+export const Input = ({ payloadsKey }: InputProps) => {
+  const dispatch = useAppDispatch();
+  const { salaryReducer } = useAppSelector(
+    (state) => state
+  );
+  const { getSalary, changeSalaryDate } = salaryActions;
+
+  const handlerInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const val = Number(e.currentTarget.value);
+
+    if (payloadsKey === "month" || payloadsKey === "year") {
+      dispatch(changeSalaryDate({ [payloadsKey]: val }));
+    } else {
+      dispatch(getSalary({ [payloadsKey]: val }));
+    }
+  };
+
+  return (
+    <input
+      placeholder={payloadsKey}
+      type='number'
+      onChange={handlerInputChange}
+      value={String(salaryReducer[payloadsKey])}
+    />
+  );
+};
