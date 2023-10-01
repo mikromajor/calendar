c4c7c2104b5006b3bf770f06e21585e42265ec42
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2023-10-01 15:06:32 +0200

    add extra TODO

--- src/components/AlcoCalendar/AlcoCalendar.tsx
+++ src/components/AlcoCalendar/AlcoCalendar.tsx
@@ -1,20 +1,4 @@
-import { useAppSelector } from "../../store/hooks/redux";
-import { alcoActions } from "../../store/reducer/alcoReducer";
 import { ControlPanel, Display, Cleaner } from "./..";
-
-//  TODO:
-// write a simple calculator with the next fields that you can change
-// volume of liquid drunk
-// quantity percent alcohol
-// Button enter
-// Calculator must adds quantity clear alcohol per month and show:
-// volume of drunk clear alcohol
-// equivalent volume this alcohol in 40% (vodka)
-// equivalent volume this alcohol in 5% (beer)
-
-//issue:
-// I can see and change only current month data
-// fix=> add input for changing months
 
 export const AlcoCalendar = () => {
   return (
@@ -25,3 +9,16 @@ export const AlcoCalendar = () => {
     </div>
   );
 };
+
+//  TODO:
+// write a simple calculator with the next fields that you can change
+// volume of liquid drunk
+// quantity percent alcohol
+// Button enter
+// Calculator must adds quantity clear alcohol per month and show:
+// volume of drunk clear alcohol per current month
+// equivalent volume this alcohol in 40% (vodka) per current month
+
+//extra TODO:
+// add info about volume of drunk clear alcohol per current year
+// add info about volume of drunk vodka per current year
