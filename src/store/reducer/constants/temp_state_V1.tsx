53670621b48649dc45eddc03ca97544a027f56fa
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2023-12-23 22:20:30 +0100

    relocate temp_state_V1 to constants

--- src/store/reducer/constants/temp_state_V1.tsx
+++ src/store/reducer/constants/temp_state_V1.tsx
@@ -21,3 +21,27 @@ const SALARY_INIT = {
   extraSalary: 0,
   totalSalary: 0,
 };
+
+const INIT_MONTH_DATA = {
+  month: "1",
+  sumEthanolPerMonth: 0,
+  sumVodkaPerMonth: 0,
+};
+const INIT_ALCO_STATE = {
+  currentYear: "2023",
+  currentMonth: "1",
+
+  sumEthanolPerYear: 0,
+  sumEthanolPerMonth: 0,
+
+  sumVodkaPerMonth: 0,
+  sumVodkaPerYear: 0,
+
+  volumeDrunks: 0,
+  percentDrunk: 5,
+
+  currentIndex: 1,
+  monthsData: [INIT_MONTH_DATA],
+
+  currentLang: "en",
+};
