4faa68b0a4de34f6838c4416f584db463c2bf915
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2024-01-26 23:22:45 +0100

    can't relocate to vite - error code ERESOLVE

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { AlcoCalendar, Salary } from "./components";
 
 //TODO: first users feedback
-// ok 1-исправить баг переключения языка при смене года
+//ok 1-исправить баг переключения языка при смене года
 //ok 2-добавить значения величин в контекст
 //ok 3-исправить перевод контекста
 //ok 4-изменить коэффициенты на боле понятные пользователю
-//5-relocate to vite or ...
+//nok 5-relocate to vite or ... (error)
 
 function App() {
   return (
