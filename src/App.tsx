e451a22bc0341c43b64a2e16b86548d3ee48770b
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2024-02-03 01:35:22 +0100

    removed todo comment from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import { AlcoCounter, Salary } from "./components";
 
-//TODO: first users feedback
-//ok 1-исправить баг переключения языка при смене года
-//ok 2-добавить значения величин в контекст
-//ok 3-исправить перевод контекста
-//ok 4-изменить коэффициенты на боле понятные пользователю
-//nok 5-relocate to vite or ... (error)
-
 function App() {
   return (
     <>
