f783d9a091c5dc6c6178815a9b887c3f5a52ea26
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2024-04-21 23:20:25 +0200

    right placed top-menu

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ function App() {
   let theme = "white-theme";
   return (
     <div className={`app app--${theme}`}>
-      <TopMenu
-        switchCalc={switchCalc}
-        setSwitchCalc={setSwitchCalc}
-      />
+      <div className='app__top-menu'>
+        <TopMenu
+          switchCalc={switchCalc}
+          setSwitchCalc={setSwitchCalc}
+        />
+      </div>
       {switchCalc ? <AlcoCounter /> : <Salary />}
     </div>
   );
