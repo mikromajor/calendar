05a0a23967c2345924d0fb29c2c97d9224fe8d21
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2024-04-21 22:36:37 +0200

    replace addButton

--- src/components/AlcoCounter/alcoComponents/ControlPanel/ControlPanel.tsx
+++ src/components/AlcoCounter/alcoComponents/ControlPanel/ControlPanel.tsx
@@ -1,13 +1,8 @@
 import { Button } from "@mui/material";
 import { ALCO_CONTENT } from "../../../../constants/alcoConstants";
-import { useState } from "react";
 import { InputDatePanel } from "./InputsPanel/InputDatePanel";
 import { InputLiquidPanel } from "./InputsPanel/InputLiquidPanel";
-import { alcoActions } from "../../../../store/reducer/alcoReducer";
-import {
-  useAppSelector,
-  useAppDispatch,
-} from "../../../../store/hooks/redux";
+import { useAppSelector } from "../../../../store/hooks/redux";
 
 type ControlPanelProps = {
   setShowAlcoCalendar: React.Dispatch<
@@ -20,20 +15,10 @@ export const ControlPanel = ({
   setShowAlcoCalendar,
   showPanelInputDate,
 }: ControlPanelProps) => {
-  const [volumeDrank, setVolumeDrank] = useState("500");
-  const [percent, setPercent] = useState("5");
-
-  const { calculating } = alcoActions;
-
   let theme = "white-theme";
   const { currentLang } = useAppSelector(
     (state) => state.appReducer
   );
-  const dispatch = useAppDispatch();
-
-  const handleCalculating = () =>
-    dispatch(calculating([volumeDrank, percent]));
-
   return (
     <div
       className={`control-panel control-panel--${theme}`}
@@ -60,22 +45,7 @@ export const ControlPanel = ({
           </Button>
         </div>
 
-        <InputLiquidPanel
-          volumeDrank={volumeDrank}
-          setVolumeDrank={setVolumeDrank}
-          percent={percent}
-          setPercent={setPercent}
-        />
-      </div>
-
-      <div className='control-panel__add-btn'>
-        <Button
-          id='btnAdd'
-          variant='contained'
-          onClick={handleCalculating}
-        >
-          {ALCO_CONTENT[currentLang].btnAdd}
-        </Button>
+        <InputLiquidPanel />
       </div>
 
       {/* <button onClick={(e) => dispatch(asyncAdder())}>
--- src/components/AlcoCounter/alcoComponents/ControlPanel/InputsPanel/InputLiquidPanel.tsx
+++ src/components/AlcoCounter/alcoComponents/ControlPanel/InputsPanel/InputLiquidPanel.tsx
@@ -1,26 +1,25 @@
-import { useAppSelector } from "../../../../../store/hooks/redux";
+import { useState } from "react";
+import { Button } from "@mui/material";
+import { alcoActions } from "../../../../../store/reducer/alcoReducer";
+import {
+  useAppSelector,
+  useAppDispatch,
+} from "../../../../../store/hooks/redux";
 import { ALCO_CONTENT } from "../../../../../constants/alcoConstants";
 import { InputLiquid } from "../Inputs";
 
-type InputLiquidPanelProps = {
-  volumeDrank: string;
-  setVolumeDrank: React.Dispatch<
-    React.SetStateAction<string>
-  >;
-  percent: string;
-  setPercent: React.Dispatch<React.SetStateAction<string>>;
-};
+export const InputLiquidPanel = () => {
+  const [volumeDrank, setVolumeDrank] = useState("500");
+  const [percent, setPercent] = useState("5");
 
-export const InputLiquidPanel = ({
-  volumeDrank,
-  setVolumeDrank,
-  percent,
-  setPercent,
-}: InputLiquidPanelProps) => {
-  let theme = "white-theme";
+  const { calculating } = alcoActions;
   const { currentLang } = useAppSelector(
     (state) => state.appReducer
   );
+  const dispatch = useAppDispatch();
+
+  const handleCalculating = () =>
+    dispatch(calculating([volumeDrank, percent]));
 
   return (
     <div className='control-panel__inputs-panel control-panel__inputs-panel--direction-column'>
@@ -37,6 +36,15 @@ export const InputLiquidPanel = ({
         setVal={setPercent}
         label={ALCO_CONTENT[currentLang].lblPercent}
       />
+      <div className='control-panel__add-btn'>
+        <Button
+          id='btnAdd'
+          variant='contained'
+          onClick={handleCalculating}
+        >
+          {ALCO_CONTENT[currentLang].btnAdd}
+        </Button>
+      </div>
     </div>
   );
 };
