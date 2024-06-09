68ad26d4fda72a7b384317ac2c046583a7c68e97
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2024-06-09 22:15:01 +0200

    stretched bgc to full height , increased the height of the cells td no-inputs

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import store from "./store";
 import "./index.scss";
 import { createRoot } from "react-dom/client";
 
-const container = document.getElementById("app");
+const container = document.getElementById("app-container");
 const root = createRoot(container!);
 
 root.render(
@@ -13,30 +13,30 @@ root.render(
   </Provider>
 );
 
-const MONTHS = {
-  jan: { days: 31, workHours: 176 },
-  feb: { days: 28, workHours: 152 },
-};
-interface M {
-  day: number;
-  workHours: number;
-}
+// const MONTHS = {
+//   jan: { days: 31, workHours: 176 },
+//   feb: { days: 28, workHours: 152 },
+// };
+// interface M {
+//   day: number;
+//   workHours: number;
+// }
 
-type Auto<O extends object, V> = {
-  [keys in keyof O]: V;
-};
-type MonthsType = typeof MONTHS;
-type Months = Auto<MonthsType, M>;
-type MonthsNames = keyof Months;
+// type Auto<O extends object, V> = {
+//   [keys in keyof O]: V;
+// };
+// type MonthsType = typeof MONTHS;
+// type Months = Auto<MonthsType, M>;
+// type MonthsNames = keyof Months;
 
-function getWorkOurs(
-  monthName: MonthsNames,
-  yearData: MonthsType
-) {
-  return yearData[monthName].workHours;
-}
+// function getWorkOurs(
+//   monthName: MonthsNames,
+//   yearData: MonthsType
+// ) {
+//   return yearData[monthName].workHours;
+// }
 
-const workHours = getWorkOurs("jan", MONTHS);
+// const workHours = getWorkOurs("jan", MONTHS);
 //MonthsType =
 // {
 //     jan: M;
