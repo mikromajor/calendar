1e48e2e478943309c6c100f2684edbeba9fd022b
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2024-07-07 15:40:34 +0200

    add routes

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+const sequelize = require("./db");
+// const models = require('./models/models')
+// const router = require('./routes/index')
+//const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// app.use("/api", router);
+// app.use(errorHandler)
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () =>
+      console.log(`Server started on port ${PORT}`)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+const Router = require("express");
+const router = new Router();
+
+// const userRouter = require('./userRouter')
+// const salaryRouter = require('./salaryRouter')
+// const calendarRouter = require('./calendarRouter')
+
+// router.use('/user', userRouter)
+// router.use('/salary', salaryRouter)
+// router.use('/calendar', calendarRouter)
+
+module.exports = router;
