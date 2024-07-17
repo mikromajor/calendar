c15a9e5e8d039c48842a203884a4433d303cd7bf
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2024-07-18 01:09:52 +0200

    add func getLast_2years

--- server/routes/salaryRouter.js
+++ server/routes/salaryRouter.js
@@ -3,8 +3,21 @@ const router = new Router();
 const salaryController = require("../controllers/salaryController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/", authMiddleware, salaryController.add);
-router.get("/", authMiddleware, salaryController.getOne);
-router.get("/", authMiddleware, salaryController.getAll);
+router.post("/set", authMiddleware, salaryController.add);
+router.get(
+  "/getOne",
+  authMiddleware,
+  salaryController.getOne
+);
+router.get(
+  "/getAll",
+  authMiddleware,
+  salaryController.getAll
+);
+router.get(
+  "/getLast_2years",
+  authMiddleware,
+  salaryController.getLast_2years
+);
 
 module.exports = router;
