71fa87fa9e87625806e55972921949967fad38e1
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2024-08-20 23:43:14 +0200

    TODO create Salary actions

--- client/src/store/reducer/http/salaryActions.ts
+++ client/src/store/reducer/http/salaryActions.ts
@@ -7,47 +7,47 @@ import {
   CurrentDate,
   AlcoState,
 } from "../../../types/alcoTypes";
+//TODO create Salary actions
+// export const getAlcoYear = createAsyncThunk(
+//   "alcoCalc/getAlcoYear",
+//   async (
+//     date: CurrentDate,
+//     { rejectWithValue, dispatch, getState }
+//   ) => {
+//     const { year, month, day } = date;
+//     try {
+//       const response = await $authHost.get<IServerRes>(
+//         `api/alco_calendar/get?year=${year}&month=${month}&day=${day}`
+//       );
 
-export const getAlcoYear = createAsyncThunk(
-  "alcoCalc/getAlcoYear",
-  async (
-    date: CurrentDate,
-    { rejectWithValue, dispatch, getState }
-  ) => {
-    const { year, month, day } = date;
-    try {
-      const response = await $authHost.get<IServerRes>(
-        `api/alco_calendar/get?year=${year}&month=${month}&day=${day}`
-      );
+//       return response.data;
+//     } catch (error: any) {
+//       if (error?.response) {
+//         return rejectWithValue(error.response.data);
+//       }
+//       return error;
+//     }
+//   }
+// );
 
-      return response.data;
-    } catch (error: any) {
-      if (error?.response) {
-        return rejectWithValue(error.response.data);
-      }
-      return error;
-    }
-  }
-);
+// export const addNewDoseToDB = createAsyncThunk(
+//   "alcoCalc/addNewDoseToDB",
+//   async (
+//     newDose: IDose,
+//     { rejectWithValue, dispatch, getState }
+//   ) => {
+//     try {
+//       const response = await $authHost.post<IServerRes>(
+//         "api/alco_calendar/add",
+//         newDose
+//       );
 
-export const addNewDoseToDB = createAsyncThunk(
-  "alcoCalc/addNewDoseToDB",
-  async (
-    newDose: IDose,
-    { rejectWithValue, dispatch, getState }
-  ) => {
-    try {
-      const response = await $authHost.post<IServerRes>(
-        "api/alco_calendar/add",
-        newDose
-      );
-
-      return response.data;
-    } catch (error: any) {
-      if (!error.response) {
-        throw error;
-      }
-      return rejectWithValue(error.response.data);
-    }
-  }
-);
+//       return response.data;
+//     } catch (error: any) {
+//       if (!error.response) {
+//         throw error;
+//       }
+//       return rejectWithValue(error.response.data);
+//     }
+//   }
+// );
